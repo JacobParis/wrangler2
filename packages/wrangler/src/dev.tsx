6249004338eec9af18fb4532c4020f2f46229e70
Sunil Pai <threepointone@gmail.com>
2022-01-01 23:07:46 +0000

    stray commented out line

--- packages/wrangler/src/dev.tsx
+++ packages/wrangler/src/dev.tsx
@@ -657,7 +657,6 @@ function useProxy({
   publicRoot: void | string;
   port: number;
 }) {
-  // const tokenRef = useRef<void |{host:string, value:string}>(token);
   useEffect(() => {
     if (!token) return;
     // TODO(soon): since headers are added in callbacks, the server
